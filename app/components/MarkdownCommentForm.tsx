94a7107d737ce55099900358cea3b6e817176f83
scossar <scossar3@gmail.com>
2024-04-01 14:23:42 -0700

    commit before moving to the formatting_buttons branch

--- app/components/MarkdownCommentForm.tsx
+++ app/components/MarkdownCommentForm.tsx
@@ -53,6 +53,14 @@ export default function MarkdownCommentForm({ ...props }: CommentFormProps) {
     debouncedPreview(raw);
   }
 
+  function handleBoldButton(event: React.FormEvent<HTMLButtonElement>) {
+    // clicking the button should either insert `**|**` into the adjacent `textarea` (with `|` representing the position of the cursor)
+    // or it should capture any text that's been highlighted in the `textarea` and surround that text with `**{highlighted_text}**`
+    // some issues to be dealt with:
+    //  - how to maintain the cursor position in the `textarea` when the button (that's outside of the `textarea`) is clicked
+    //  - how to capture the `textarea` highlighted text in the `handleBoldButton` event handler
+  }
+
   return (
     <div className={className}>
       <div
@@ -74,6 +82,14 @@ export default function MarkdownCommentForm({ ...props }: CommentFormProps) {
               />
             ))}
         </>
+        <div className="formatting-buttons flex w-full bg-slate-100">
+          <button
+            className="px-2 py-1 m-1 bg-slate-50 text-slate-900 border border-slate-900 rounded-md"
+            onClick={handleBoldButton}
+          >
+            Bold
+          </button>
+        </div>
         <textarea
           className="h-96 p-2 text-slate-950"
           name="rawComment"
