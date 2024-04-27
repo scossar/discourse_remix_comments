ecc12370bb1444d18b31792502ea166887b311b3
scossar <scossar3@gmail.com>
2024-04-26 23:43:58 -0700

    add currentUser.server.ts

--- app/routes/api.getTopicComments.tsx
+++ app/routes/api.getTopicComments.tsx
@@ -1,17 +1,30 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { discourseEnv } from "~/services/config.server";
 import { discourseSessionStorage } from "~/services/session.server";
-import { CurrentUserSchema, CurrentUser } from "~/schemas/currentUser";
+import { getSessionData, validateSession } from "~/schemas/currentUser.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  const env = discourseEnv();
-  console.log(`apiKey: ${env.apiKey}`);
   const session = await discourseSessionStorage.getSession(
     request.headers.get("Cookie")
   );
 
-  const { externalId, avatarUrl, admin, username } = session.get();
+  const sessionData = getSessionData(session);
+  let currentUser;
+  try {
+    currentUser = validateSession(sessionData);
+  } catch (error) {
+    throw new Response("Something has gone wrong", { status: 403 });
+  }
 
-  console.log(`externalId: ${externalId}`);
-  return null;
+  console.log(`currentUser: ${JSON.stringify(currentUser, null, 2)}`);
+
+  return json(
+    {},
+    {
+      headers: {
+        "Set-Cookie": await discourseSessionStorage.commitSession(session),
+      },
+    }
+  );
 }
