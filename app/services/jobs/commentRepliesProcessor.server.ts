33323f4db9ad0b115fc1ad1616b0d47330559d72
scossar <scossar3@gmail.com>
2024-05-18 16:52:41 -0700

    set postRepliesKey

--- app/services/jobs/commentRepliesProcessor.server.ts
+++ app/services/jobs/commentRepliesProcessor.server.ts
@@ -1,4 +1,10 @@
 import { discourseEnv } from "~/services/config.server";
+import QueueError from "~/services/errors/queueError.server";
+import { validateDiscourseApiReplyPosts } from "~/schemas/discourseApiResponse.server";
+import { getRedisClient } from "~/services/redisClient.server";
+import { getPostRepliesKey } from "~/services/redisKeys.server";
+import type { ParsedDiscourseCommentReplies } from "~/types/parsedDiscourse";
+import { transformReplyPost } from "../transformDiscourseDataZod.server";
 
 export async function commentRepliesProcessor(
   postId: number,
@@ -11,4 +17,33 @@ export async function commentRepliesProcessor(
   headers.append("Api-Username", username ? username : "system");
 
   const url = `${baseUrl}/posts/${postId}/replies.json`;
+
+  const response = await fetch(url, { headers });
+
+  if (!response.ok) {
+    throw new QueueError(
+      "Unable to fetch replies from Discourse",
+      response.status
+    );
+  }
+
+  const json = await response.json();
+  const validReplyPosts = validateDiscourseApiReplyPosts(json);
+
+  const postReplies: ParsedDiscourseCommentReplies = {
+    repliesForPostId: postId,
+    posts: validReplyPosts.map((post) => transformReplyPost(post, baseUrl)),
+  };
+
+  const stringifiedReplyPosts = JSON.stringify(postReplies);
+
+  let client;
+  try {
+    client = await getRedisClient();
+    await client.set(getPostRepliesKey(postId), stringifiedReplyPosts);
+  } catch (error) {
+    throw new QueueError(`Unable to set postReplies for postId: ${postId}`);
+  }
+
+  return stringifiedReplyPosts;
 }
