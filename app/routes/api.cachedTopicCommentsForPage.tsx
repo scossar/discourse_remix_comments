1fa47b10df3df3ade89fb98cbcaa663514646ebf
scossar <scossar3@gmail.com>
2024-05-22 10:08:05 -0700

    delete console.log statement

--- app/routes/api.cachedTopicCommentsForPage.tsx
+++ app/routes/api.cachedTopicCommentsForPage.tsx
@@ -13,11 +13,6 @@ export async function loader({ request }: LoaderFunctionArgs) {
     const cacheKey = getTopicCommentsKey(topicId, page);
     const stringifiedComments = await client.get(cacheKey);
     if (stringifiedComments) {
-      console.log(
-        `getting cached comments for topicId: ${topicId}, page: ${page}`
-      );
-
-      console.log(JSON.parse(stringifiedComments));
       return JSON.parse(stringifiedComments);
     }
     // TODO: probably call `addTopicCommentsRequest({topicId, page})`
