5fb3f440517957ef79057bf6f8ca30965ea8ca8f
scossar <scossar3@gmail.com>
2024-05-24 16:10:47 -0700

    commit before closing project for a bit

--- app/services/jobs/commentProcessor.server.ts
+++ app/services/jobs/commentProcessor.server.ts
@@ -2,13 +2,18 @@
 // TODO: re-enable no-unused-vars
 
 import { type DiscourseApiWebHookPost } from "~/schemas/discourseApiResponse.server";
+import { transformPost } from "~/services/transformDiscourseDataZod.server";
+import { type DiscourseRawEnv, discourseEnv } from "~/services/config.server";
 
 type CommentProcessorArgs = {
   topicId: number;
   postId: number;
-  postJson: DiscourseApiWebHookPost;
+  webHookJson: DiscourseApiWebHookPost;
 };
 
 export async function commentProcessor(args: CommentProcessorArgs) {
-  const { topicId, postId, postJson } = args;
+  const { baseUrl } = discourseEnv();
+  const { topicId, postId, webHookJson } = args;
+  const tranformedPost = transformPost(webHookJson.post, baseUrl);
+  console.log(`transformedPost, ${JSON.stringify(transformPost, null, 2)}`);
 }
