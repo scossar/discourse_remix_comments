f6a4f230b72ee7e39f909a75574eebf6c8f84e7a
scossar <scossar3@gmail.com>
2024-05-28 20:29:53 -0700

    delete unused file

--- app/services/parseAndCacheTopicComments.server.ts
+++
@@ -1,8 +0,0 @@
-import { addTopicStreamRequest } from "~/services/jobs/postStreamWorker.server";
-export async function parseAndCacheTopicComments(topicId: number) {
-  try {
-    await addTopicStreamRequest({ topicId: topicId });
-  } catch (error) {
-    throw new Error("error message needed");
-  }
-}
